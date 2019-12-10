74e490b65cc920af973992997a322d9e9040a7f3
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-10 18:05:38 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,21 @@
 # Use this space to code along with the readme
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
+say_hello_world_ten_times
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+# ... The rest of the program
+
+
+say_hello_world_ten_times
